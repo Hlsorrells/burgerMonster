c7397919c19094c69b05e6058bdbb81cb4544b85
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-10 21:16:08 -0400

    create orm functions to interact with db

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+// =================================================================
+// DEPENDENCIES
+// =================================================================
+
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const cat = {
+  all: (cb) => {
+    orm.all("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: (cols, vals, cb) => {
+    orm.create("burgers", cols, vals, (res) => {
+      cb(res);
+    });
+  },
+  update: (objColVals, condition, cb) => {
+    orm.update("burgers", objColVals, condition, (res) => {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (controller.js).
+module.exports = burger;
