015d297065129e602da00d3144d16e9ef0277fc3
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-10 22:02:32 -0400

    added delete route & modified conditions to template literals

--- controllers/controller.js
+++ controllers/controller.js
@@ -36,7 +36,7 @@ router.post("/api/burgers", (req, res) => {
 });
 
 router.put("/api/burgers/:id", (req, res) => {
-  const condition = "id = " + req.params.id;
+  const condition = `id = ${req.params.id}`;
 
   console.log("condition", condition);
 
@@ -44,6 +44,19 @@ router.put("/api/burgers/:id", (req, res) => {
     devoured: req.body.devoured
   }, condition, (result) => {
     if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  cat.delete(condition, (result) => {
+    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
