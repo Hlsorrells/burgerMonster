a46db4477e5c1f77a61b0a3e63cc51bd356425ad
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-11 16:53:47 -0400

    removed unused delete function

--- models/burger.js
+++ models/burger.js
@@ -5,7 +5,7 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-const cat = {
+const burger = {
   all: (cb) => {
     orm.all("burgers", (res) => {
       cb(res);
@@ -19,11 +19,6 @@ const cat = {
   },
   update: (objColVals, condition, cb) => {
     orm.update("burgers", objColVals, condition, (res) => {
-      cb(res);
-    });
-  },
-  delete: (condition, cb) => {
-    orm.delete("burgers", condition, (res) => {
       cb(res);
     });
   }
