4e2f14ab69dd734c32b6a17986c0455f40d9eb40
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-11 19:27:00 -0400

    added remote connection to heroku/JawsDB

--- config/connection.js
+++ config/connection.js
@@ -12,13 +12,19 @@ const mysql = require("mysql");
 // =================================================================
 
 // MySQL DB Connection Information
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgerMonster_db"
-});
+const connection 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgerMonster_db"
+    });
+}
+
 
 // Initiate MySQL Connection.
 connection.connect((err) => {
